ea3a10cdd0ac7282074f298d898c4608275e9829
= <=>
2023-07-18 21:22:30 +0800

    添加投影坐标转换

--- OpenGL/src/Application.cpp
+++ OpenGL/src/Application.cpp
@@ -14,6 +14,9 @@
 #include "Shader.h"
 #include "Texture.h"
 
+#include "glm/glm.hpp"
+#include "glm/gtc/matrix_transform.hpp"
+
 int main(void)
 {
     {
@@ -28,7 +31,7 @@ int main(void)
         glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
         /* Create a windowed mode window and its OpenGL context */
-        window = glfwCreateWindow(640, 640, "Hello World", NULL, NULL);
+        window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
         if (!window)
         {
             glfwTerminate();
@@ -44,16 +47,20 @@ int main(void)
             return -1;
 
         float positions[] = {
-            -0.5f, -0.5f, 0.0f, 0.0f,
-            0.5f, -0.5f, 1.0f, 0.0f,
-            0.5f, 0.5f, 1.0f, 1.0f,
-            -0.5f, 0.5f, 0.0f, 1.0f,
+            100.0f, 100.0f, 0.0f, 0.0f,
+            200.0f, 100.0f, 1.0f, 0.0f,
+            200.0f, 200.0f, 1.0f, 1.0f,
+            100.0f, 200.0f, 0.0f, 1.0f,
         };
 
         unsigned int indices[6]{
             0, 1, 2,
             2, 3, 0
         };
+
+        glm::mat4 proj = glm::ortho(0.0f, 640.0f, 0.0f, 480.0f, -1.0f, 1.0f);
+        glm::vec4 vp(100.0f, 100.0f, 0.0f, 1.0f);
+        glm::vec4 result = proj * vp;//这里的结果可以看出，把vp坐标转换为相对于屏幕坐标系(-1~1,-1`1)的坐标
 
         GLCALL(glEnable(GL_BLEND));
         GLCALL(glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA));
@@ -70,11 +77,13 @@ int main(void)
 
         Shader shader("resources/shaders/BasicShader.shader");
         shader.Bind();
-        shader.SetUniform4f("u_Color", 0.0f, 0.5f, 0.0f, 1.0f);
+        //shader.SetUniform4f("u_Color", 0.0f, 0.5f, 0.0f, 1.0f);
 
         Texture texture("resources/textures/ChernoLogo.png");
         texture.Bind(0);
         shader.SetUniform1i("u_Texture", 0);
+
+        shader.SetUniformMat4f("u_MVP", proj);
 
         Renderer renderer;
 
